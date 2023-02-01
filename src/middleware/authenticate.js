a77d0c7b6627ae7d41321bfa3eca229a64feabca
ngoma <ngomachrs@gmail.com>
2023-02-01 15:50:55 +0200

    maintainability
    Test coverage

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -1,33 +1,26 @@
 import jwt from "jsonwebtoken";
 
-const authenticate = (req,res,next) =>{
-    try{
-        const token = req.headers.authorization.split(" ")[1]
-        const decode= jwt.verify(token, "AzQ,PI)0(")
-
-        req.user = decode
-        next()
-    }catch(error){
-        res.json({
-            message: "Authentication Failed! Login first"
-        })
-    }
+const authenticate = (req, res, next) => {
+    tkn(req,res,"AzQ,PI)0(",next);
 }
-const admin = (req,res,next) =>{
-    try{
-        let tkn = req.headers.authorization.split(" ")[1]
-        let decoded= jwt.verify(tkn, "ngoma)(")
-        req.user = decoded
-        next()
-    }catch(err){
-        res.send({
+const admin = (req, res, next) => {
+tkn(req,res,"ngoma)(",next);
+}
+
+function tkn(req,res,key,next){
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+            const decode = jwt.verify(token, key)
+            req.user = decode
+            next()
+    } catch (error) {
+        res.json({
             message: "Authentication Failed!"
         })
     }
 }
 
-
 module.exports = {
     authenticate,
     admin
-     };
+};
